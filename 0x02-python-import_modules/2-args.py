d32ef1db279bf4cb4e963e31d3509f11194d51fa
amoskarugo <karugoamos@gmail.com>
2023-08-11 20:51:50 +0000

    command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     if args_count > 1:
         print("{} argument:".format(args_count - 1) if args_count == 2 else
-              "{} arguments".format(args_count - 1))
+              "{} argument:".format(args_count - 1))
         for arg in range(1, args_count):
             print("{}: {}".format(arg, sys.argv[arg]))
     else:
