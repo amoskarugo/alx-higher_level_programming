3b0880f21871f2d7f111a12d673c4de6012c5a49
amoskarugo <karugoamos@gmail.com>
2023-08-11 18:49:49 +0000

    import add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,6 +4,6 @@ a = 1
 b = 2
 if __name__ == "__main__":
     from add_0 import add
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{a} + {b} = {add(a, b)}")
 else:
     __import__("add_0")
