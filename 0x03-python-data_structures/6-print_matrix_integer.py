77641fa8460b22b03166c213063659a470248e3e
amoskarugo <karugoamos@gmail.com>
2023-08-14 19:09:59 +0000

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.8
+
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" ")
+        print()
