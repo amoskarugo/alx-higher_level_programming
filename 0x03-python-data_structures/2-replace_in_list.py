abbf2e88adcbc2be0a61860285b8b504d0fb9c92
amoskarugo <karugoamos@gmail.com>
2023-08-14 07:13:33 +0000

    replace at index

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.8
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+
+    list_len = len(my_list) - 1
+    if idx > list_len:
+        return my_list
+
+    my_list[idx] = element
