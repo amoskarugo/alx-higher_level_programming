1cd0f665981072ae2711056e2572cfad2c38870a
amoskarugo <karugoamos@gmail.com>
2023-08-14 18:56:35 +0000

    #

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.8
+
+def no_c(my_string):
+    if not my_string or my_string == " ":
+        return
+    new_string = ""
+
+    for char in my_string:
+        if char in "cC":
+            continue
+        new_string += char
+    return new_string
