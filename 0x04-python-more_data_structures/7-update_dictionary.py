caff10f92d2a9b04ee16f02cda0f84ba8afc314c
amoskarugo <karugoamos@gmail.com>
2023-08-17 13:22:49 +0000

    update dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3.8
 
 def update_dictionary(a_dictionary, key, value):
-    for i, j in sorted(a_dictionary.items()):
-        if i == key:
-            a_dictionary[i] = value
-        else:
-            a_dictionary[key] = value
+    a_dictionary[key] = value
     return a_dictionary
+
