b409c41324691ef19b41b15dbdbbd5491400b1ba
amoskarugo <karugoamos@gmail.com>
2023-09-11 18:06:38 +0000

    #

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Geometry class module"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
