5842bbfcc8f9967da970c7f85cdab956423ec32c
amoskarugo <karugoamos@gmail.com>
2023-09-11 20:27:00 +0000

    #

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,14 +3,25 @@
 
 
 class BaseGeometry:
-    """Empty class"""
+    """A base class for geometry-related operations"""
     def __init__(self):
         pass
 
     def area(self):
+        """calculates area"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """checks whether the value passed is an integer.
+
+        Args:
+            name (string): name of the integer to be passed
+            value (int): parameter to check whether it is an integer
+
+        Raises:
+            TypeError: if the value is not an integer
+            ValueError: if the value is less or equal to zero
+        """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
 
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Geometry class module"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """BaseGeometry derived class"""
+
+    def __init__(self, width, height):
+        """object attributes initialization"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
