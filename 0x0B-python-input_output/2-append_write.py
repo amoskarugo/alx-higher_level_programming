ec306db4929cd464831e7ab8fe2ef2d6120609ff
amoskarugo <karugoamos@gmail.com>
2023-09-13 09:52:45 +0000

    #

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,7 @@
+#!/usr/bin/python3.8
+"""module that handles appending into a file."""
 
+
+def append_write(filename="", text=""):
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(text)
