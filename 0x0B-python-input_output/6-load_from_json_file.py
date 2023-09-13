897239ab580d9236264642bd4e5cdaa4c2b6df50
amoskarugo <karugoamos@gmail.com>
2023-09-13 11:36:08 +0000

    #

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+"""module load from json file."""
+import json
 
+
+def load_from_json_file(filename):
+    """loads json data from a file"""
+    with open(filename, "r", encoding="utf-8") as f:
+        x = json.load(f)
+    return json.loads(x)
