1d4c720985f7fcec21789cba0e8129bfab816828
amoskarugo <karugoamos@gmail.com>
2023-09-13 10:47:42 +0000

    #

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,7 @@
+#!/usr/bin/python3
+"""module to convert string to data structure."""
 
+
+def from_json_string(my_str):
+    """converts a JSON string to an object(python data structure)."""
+    return json.loads(my_str)
