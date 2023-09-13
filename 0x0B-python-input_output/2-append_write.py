e6693332951f5295bd06ff554c76c2575afcb8c9
amoskarugo <karugoamos@gmail.com>
2023-09-13 10:04:12 +0000

    #

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,9 @@
 
 
 def append_write(filename="", text=""):
+    """function that appends into the file and returns
+       the number of characters appended
+    """
     with open(filename, "a", encoding="utf-8") as f:
         appended = f.write(text)
     return appended
