7e0ff37fe0d5e647824e5254165e117c08344606
amoskarugo <karugoamos@gmail.com>
2023-09-18 21:03:00 +0000

    unittests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Defines unittests for base.py.
 
-"""module for base class unit test"""
-
+Unittest classes:
+    TestBase_instantiation - line 21
+    TestBase_to_json_string - line 108
+    TestBase_save_to_file - line 154
+    TestBase_from_json_string - line 232
+    TestBase_create - line 286
+    TestBase_load_from_file - line 338
+    TestBase_save_to_file_csv - line 404
+    TestBase_load_from_file_csv - line 482
+"""
 from models.rectangle import Rectangle
 from models.base import Base
 from models.square import Square
