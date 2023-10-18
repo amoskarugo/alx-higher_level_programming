bfebc23a05635d39c05f9fe4c074bef5e484ffdb
amoskarugo <karugoamos@gmail.com>
2023-10-18 11:44:19 +0000

    #

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,4 @@
---show all tables of a database
---database name will be passed as mysql command
+-- show all tables of a database.
+-- database name will be passed as mysql command.
+
 SHOW TABLES;
