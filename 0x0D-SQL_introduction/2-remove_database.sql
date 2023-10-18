9b574a3d9bf663c63653f3c41026965833f3f9f0
amoskarugo <karugoamos@gmail.com>
2023-10-18 13:56:25 +0300

    #

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- delete database hbtn_0c_0 in MYSQL server if it exists.
+DROP DATABASE IF EXISTS hbtn_0c_0;
+
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+--insert into the table
+INSERT INTO first_table(id, name)
+VALUES(89, "Best School");
+
