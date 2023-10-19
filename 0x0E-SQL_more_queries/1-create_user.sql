6ef79be9ccb36749df49ee01aa9ee65419314ea1
amoskarugo <karugoamos@gmail.com>
2023-10-19 20:00:13 +0000

    create & grant privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- create user 'user_0d_1' if the user does not exist.
+-- should have all the privileges.
+-- set password to 'user_0d_1_pwd'
+
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
