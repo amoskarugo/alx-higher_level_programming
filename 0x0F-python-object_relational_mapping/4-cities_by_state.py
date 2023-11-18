77ffa271f53dc487cf8abc08735e01e3e098af08
amoskarugo <karugoamos@gmail.com>
2023-11-18 18:31:10 +0000

    join

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""python script that connects to MySQL db and
+fetches data. """
+import MySQLdb
+import sys
+
+args = sys.argv
+
+
+def list_():
+    """function fetches filtered data and prints it.
+        print all items on the screen
+    """
+    db = MySQLdb.connect(host='localhost', user=args[1],
+                         passwd=args[2], db=args[3], port=3306)
+    cur = db.cursor()
+
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                FROM cities
+                JOIN states
+                ON states.id = cities.state_id
+                ORDER BY cities.id ASC"""
+                )
+    results = cur.fetchall()
+
+    for state in results:
+        print(state)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    list_()
