84444fd98504b3bddf9529174aba35fd2cfabb15
amoskarugo <karugoamos@gmail.com>
2023-11-21 06:49:43 +0000

    fetch 1st object

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+script that fetches the first object in the table
+using SQLAlchemy module
+"""
+
+
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    import sys
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           ''.format(sys.argv[1], sys.argv[2],
+                                     sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+    session = Session()
+
+    Base.metadata.create_all(engine)
+
+    first_obj = session.query(State).first()
+
+    if first_obj:
+        print("{}: {}".format(first_obj.id, first_obj.name))
+    else:
+        print()
