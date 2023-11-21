05a1692793f4ed89cea21f0c3af20b12ad02e3da
amoskarugo <karugoamos@gmail.com>
2023-11-21 06:14:28 +0000

    link class to table in database

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,25 +4,15 @@ python file that contains the class definition of a
 State and an instance Base = declarative_base()
 """
 
-if __name__ == '__main__':
-    from sqlalchemy import Sequence, String, Column, Integer
-    from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    import sys
+from sqlalchemy import Sequence, String, Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           ''.format(sys.argv[1], sys.argv[2],
-                                     sys.argv[3]), pool_pre_ping=True)
-    session_maker = sessionmaker(bind=engine)
-    session_maker.configure(bind=engine)
-    session = session_maker()
-    Base = declarative_base()
+Base = declarative_base()
 
-    class State(Base):
-        __tablename__ = 'states'
 
-        id = Column(Integer, primary_key=True)
-        name = Column(String(128))
+class State(Base):
+    """defining the state class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
 
-    Base.metadata.create_all(engine)
